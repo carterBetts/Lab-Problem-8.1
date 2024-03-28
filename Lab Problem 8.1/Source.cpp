e86b8ab1c7425a2ce3a36bf4a550e5765556d8a4
Carter Betts <c-betts@onu.edu>
2024-03-28 08:29:56 -0400

    Completed countCharecter function

--- Lab Problem 8.1/Source.cpp
+++ Lab Problem 8.1/Source.cpp
@@ -2,39 +2,14 @@
 #include <string>
 using namespace std;
 
-int countVowel(string str);
+int countCharecter(string str);
 
 int main(void)
 {
-	
+	cout << countCharecter("Northern");
 }
 
-int countVowel(string str)
+int countCharecter(string str)
 {
-	int total = 0;
-	for (int i = 0; i < str.length(); i++)
-	{
-		string charecter = str.substr(i, 1);
-		if (charecter.compare("A") == 0)
-			total++;
-		if (charecter.compare("a") == 0)
-			total++;
-		if (charecter.compare("E") == 0)
-			total++;
-		if (charecter.compare("e") == 0)
-			total++;
-		if (charecter.compare("I") == 0)
-			total++;
-		if (charecter.compare("i") == 0)
-			total++;
-		if (charecter.compare("O") == 0)
-			total++;
-		if (charecter.compare("o") == 0)
-			total++;
-		if (charecter.compare("U") == 0)
-			total++;
-		if (charecter.compare("u") == 0)
-			total++;
-	}
-	return total;
+	return str.length();
 }
